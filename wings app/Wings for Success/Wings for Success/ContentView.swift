1bca4794e9ec04bf8513fc76e0c02756e0e89578
Shivika Varshney <44555608+svarshney25@users.noreply.github.com>
2022-01-30 13:08:51 -0500

    Add files via upload

---
+++ wings app/Wings for Success/Wings for Success/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Wings for Success
+//
+//  Created by Shivika Varshney on 1/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    Section {
+                        
+                    }
+                }
+            }
+            .navigationTitle("Volunteer SignUp")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
